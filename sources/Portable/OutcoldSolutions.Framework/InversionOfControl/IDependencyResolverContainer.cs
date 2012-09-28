b3488d957b9c9126a2c05fb9348ac30aae98db13
Denis Gladkikh <outcoldman@outlook.com>
2012-09-27 22:55:55 -0700

    Add comments

--- sources/Portable/OutcoldSolutions.Framework/InversionOfControl/IDependencyResolverContainer.cs
+++ sources/Portable/OutcoldSolutions.Framework/InversionOfControl/IDependencyResolverContainer.cs
@@ -7,44 +7,63 @@ namespace OutcoldSolutions
     using System;
 
     /// <summary>
-    /// The dependency resolver container interface.
+    /// The dependency resolver container interface. Provides inversion of control practice.
     /// </summary>
+    /// <example>
+    /// <code>
+    /// IDependencyResolverContainer container = new DependencyResolverContainer();
+    /// using (var regisration = container.Registration())
+    /// {
+    ///     registration.Register(typeof(IInterfaceA)).As(typeof(ClassA));
+    /// }
+    /// var instance = (IInterfaceA)container.Resolve(typeof(IInterfaceA));
+    /// </code>
+    /// </example>
     public interface IDependencyResolverContainer : IDisposable
     {
         /// <summary>
-        /// Get registration context to register types and instances in container. This operation
-        /// blocks all resolve operation untill <see cref="IRegistrationContext"/> will be disposed.
+        /// Get registration context which allows to register types and instances for current container. This operation
+        /// blocks all resolve operation untill instance of <see cref="IRegistrationContext"/> will be disposed.
         /// </summary>
         /// <returns>
         /// The <see cref="IRegistrationContext"/>. Allows to register types and instances in container.
         /// </returns>
+        /// <exception cref="NotSupportedException">
+        /// If previos created instance of <see cref="IRegistrationContext"/> is not disposed yet.
+        /// </exception>
         IRegistrationContext Registration();
 
         /// <summary>
         /// Gets the context based dependency resolver container (child container). 
         /// </summary>
         /// <param name="context">
-        /// The context.
+        /// The context, unique identifier for child container. Cannot be null.
         /// </param>
         /// <returns>
-        /// The instance of dependency resolver container <see cref="IDependencyResolverContainer"/>.
+        /// The instance of child dependency resolver container <see cref="IDependencyResolverContainer"/>.
         /// </returns>
+        /// <exception cref="NullReferenceException">
+        /// If <paramref name="context"/> is null.
+        /// </exception>
         IDependencyResolverContainer Context(string context);
 
         /// <summary>
-        /// Check if <paramref name="type"/> is registered and can be resolved with current container.
+        /// Checks if <paramref name="type"/> is registered and can be resolved with current container.
         /// If current container doesn't have registered type it will check parent container for it.
         /// </summary>
         /// <param name="type">
         /// The type.
         /// </param>
         /// <returns>
-        /// Is current type <paramref name="type"/> is registered.
+        /// If current type <paramref name="type"/> is registered.
         /// </returns>
+        /// <exception cref="NullReferenceException">
+        /// If <paramref name="type"/> is null.
+        /// </exception>
         bool IsRegistered(Type type);
 
         /// <summary>
-        /// Check if <typeparamref name="TType"/> is registered and can be resolved with current container.
+        /// Checks if <typeparamref name="TType"/> is registered and can be resolved with current container.
         /// If current container doesn't have registered type it will check parent container for it.
         /// </summary>
         /// <typeparam name="TType">
@@ -56,19 +75,31 @@ namespace OutcoldSolutions
         bool IsRegistered<TType>();
 
         /// <summary>
-        /// Resolve instace with a type <paramref name="type"/>.
+        /// Resolve instace for a type <paramref name="type"/>. 
         /// </summary>
         /// <param name="type">The type for resolve.</param>
-        /// <param name="arguments">The list of arguments for constructor.</param>
+        /// <param name="arguments">The list of arguments for constructor of current object and all required object which should be resolved in a chain.</param>
         /// <returns>Resolved object.</returns>
+        /// <exception cref="NotSupportedException">
+        /// If container is blocked by <see cref="IRegistrationContext"/> created by <see cref="Registration"/>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <paramref name="type"/> cannot be resolved.
+        /// </exception>
         object Resolve(Type type, params object[] arguments);
 
         /// <summary>
-        /// Resolve type.
+        /// Resolve instace for a type <typeparamref name="TType"/>. 
         /// </summary>
         /// <typeparam name="TType">The type for resolve.</typeparam>
-        /// <param name="arguments">The list of arguments for constructor.</param>
-        /// <returns>Resolved object.</returns>
+        /// <param name="arguments">The list of arguments for constructor of current object and all required object which should be resolved in a chain.</param>
+        /// <returns>Instance of resolved object.</returns>
+        /// <exception cref="NotSupportedException">
+        /// If container is blocked by <see cref="IRegistrationContext"/> created by <see cref="Registration"/>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <typeparamref name="TType"/> cannot be resolved.
+        /// </exception>
         TType Resolve<TType>(params object[] arguments);
     }
 }
--- sources/Portable/OutcoldSolutions.Framework/InversionOfControl/InjectAttribute.cs
+++ sources/Portable/OutcoldSolutions.Framework/InversionOfControl/InjectAttribute.cs
@@ -7,8 +7,24 @@ namespace OutcoldSolutions
     using System;
 
     /// <summary>
-    /// The inject attribute.
+    /// The inject attribute. Use it if you class has more than one constructor to 
+    /// mark which constructor should be used for injection.
     /// </summary>
+    /// <example>
+    /// <code>
+    /// class ClassA
+    /// {
+    ///     [Inject]
+    ///     public ClassA(ClassB a)
+    ///     {
+    ///     }
+    /// 
+    ///     public ClassA(ClassB a, string str)
+    ///     {
+    ///     }
+    /// }
+    /// </code>
+    /// </example>
     [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
     public sealed class InjectAttribute : Attribute
     {
