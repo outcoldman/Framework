aa84ad4ba0a57152cbe618e0d681fe7ce04e6a4d
Denis Gladkikh <outcoldman@outlook.com>
2012-10-19 10:30:25 -0700

    Update version. Create AssemblyInfo.Common

--- sources/Portable/OutcoldSolutions.Diagnostics.ObjectReleaseVerification/Properties/AssemblyInfo.cs
+++ sources/Portable/OutcoldSolutions.Diagnostics.ObjectReleaseVerification/Properties/AssemblyInfo.cs
@@ -2,31 +2,10 @@
 // Outcold Solutions (http://outcoldman.ru)
 // --------------------------------------------------------------------------------------------------------------------
 
-using System;
 using System.Reflection;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OutcoldSolutions.Diagnostics.ObjectReleaseVerification")]
 [assembly: AssemblyDescription("Object Release Verification Framework")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Outcold Solutions")]
-[assembly: AssemblyProduct("OutcoldSolutions.Diagnostics.ObjectReleaseVerification")]
-[assembly: AssemblyCopyright("\x00a9 Outcold Solutions. All rights reserved.")]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: CLSCompliant(true)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.21014.*")]
-[assembly: AssemblyFileVersion("1.0.21014.0")]
---
+++ sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.ru)
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Outcold Solutions")]
+[assembly: AssemblyProduct("OutcoldSolutions.Diagnostics.ObjectReleaseVerification")]
+[assembly: AssemblyCopyright("\x00a9 Outcold Solutions. All rights reserved.")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: CLSCompliant(true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.21019.*")]
+[assembly: AssemblyFileVersion("1.0.21019.0")]
