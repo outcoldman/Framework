6cf65d13f14eeac34aba3b71a14619a2e6484b61
Denis Gladkikh <outcoldman@outlook.com>
2012-12-09 16:06:13 -0800

    Create version 021209: StackOverflow bug fix.

--- sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
+++ sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
@@ -23,5 +23,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.21019.*")]
-[assembly: AssemblyFileVersion("1.0.21019.0")]
+[assembly: AssemblyVersion("1.0.21209.*")]
+[assembly: AssemblyFileVersion("1.0.21209.0")]
