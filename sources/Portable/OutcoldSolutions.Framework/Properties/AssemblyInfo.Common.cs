e4f5893bfefa74895f755ab451f18c50d0ebea96
outcoldman <outcoldman@outlook.com>
2013-02-24 00:53:24 -0800

    Add new versions of libraries.

--- sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
+++ sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
@@ -21,5 +21,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.30217.*")]
-[assembly: AssemblyFileVersion("1.0.30217.0")]
+[assembly: AssemblyVersion("1.0.30224.*")]
+[assembly: AssemblyFileVersion("1.0.30224.0")]
