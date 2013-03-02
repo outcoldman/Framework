0a7e21831a20ea8b5d64b02f790b5432f565bc63
outcoldman <outcoldman@outlook.com>
2013-03-02 15:24:37 -0800

    Add ValueToBooleanConverter

---
+++ sources/Suites/OutcoldSolutions.Presentation.Suites/Converters/ValueToBooleanConverterSuites.cs
@@ -0,0 +1,209 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.com)
+// --------------------------------------------------------------------------------------------------------------------
+namespace OutcoldSolutions.Presentation.Suites.Converters
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using OutcoldSolutions.Converters;
+
+    using Windows.UI.Xaml;
+
+    public class ValueToBooleanConverterSuites : PresentationSuitesBase
+    {
+        private const string EnglishLanguage = "en";
+        private static readonly Type BooleanType = typeof(bool);
+
+        private ValueToBooleanConverter converter;
+
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            this.converter = new ValueToBooleanConverter();
+        }
+
+        [Test]
+        public void Convert_NotNull_False()
+        {
+            // Act
+            var value = this.converter.Convert(new object(), BooleanType, null, EnglishLanguage);
+
+            // Assert
+            Assert.IsFalse((bool)value);
+        }
+
+        [Test]
+        public void Convert_Null_True()
+        {
+            // Act
+            var value = this.converter.Convert(null, BooleanType, null, EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_NotNullInverted_False()
+        {
+            // Arrange
+            this.converter.Invert = true;
+
+            // Act
+            var value = this.converter.Convert(new object(), BooleanType, null, EnglishLanguage);
+
+            // Assert
+            Assert.IsFalse((bool)value);
+        }
+
+        [Test]
+        public void Convert_NullInverted_True()
+        {
+            // Arrange
+            this.converter.Invert = true;
+
+            // Act
+            var value = this.converter.Convert(null, BooleanType, null, EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_DoublesNotEqual_False()
+        {
+            // Act
+            var value = this.converter.Convert(2.0d, BooleanType, "1", EnglishLanguage);
+
+            // Assert
+            Assert.IsFalse((bool)value);
+        }
+
+        [Test]
+        public void Convert_DoublesAreEqual_True()
+        {
+            // Act
+            var value = this.converter.Convert(2.0d, BooleanType, "2", EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_DoublesNotEqualInverted_False()
+        {
+            // Arrange
+            this.converter.Invert = true;
+
+            // Act
+            var value = this.converter.Convert(2.0d, BooleanType, "1", EnglishLanguage);
+
+            // Assert
+            Assert.IsFalse((bool)value);
+        }
+
+        [Test]
+        public void Convert_DoublesAreEqualInverted_True()
+        {
+            // Arrange
+            this.converter.Invert = true;
+
+            // Act
+            var value = this.converter.Convert(2.0d, BooleanType, "2", EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_DoublesNotEqualCultureTest_False()
+        {
+            using (new CultureScope("ru"))
+            {
+                // Act
+                var value = this.converter.Convert(2.0d, BooleanType, "1.0", EnglishLanguage);
+
+
+                // Assert
+                Assert.IsFalse((bool)value);
+            }
+        }
+
+        [Test]
+        public void Convert_DoublesAreEqualCultureTest_True()
+        {
+            using (new CultureScope("ru"))
+            {
+                // Act
+                var value = this.converter.Convert(2.0d, BooleanType, "2.0", EnglishLanguage);
+
+                // Assert
+                Assert.IsTrue((bool)value);
+            }
+        }
+
+        [Test]
+        public void Convert_DoublesNotEqualInvertedCultureTest_False()
+        {
+            using (new CultureScope("ru"))
+            {
+                // Arrange
+                this.converter.Invert = true;
+
+                // Act
+                var value = this.converter.Convert(2.0d, BooleanType, "1.0", EnglishLanguage);
+
+                // Assert
+                Assert.IsFalse((bool)value);
+            }
+        }
+
+        [Test]
+        public void Convert_DoublesAreEqualInvertedCultureTest_True()
+        {
+            using (new CultureScope("ru"))
+            {
+                // Arrange
+                this.converter.Invert = true;
+
+                // Act
+                var value = this.converter.Convert(2.0d, BooleanType, "2.0", EnglishLanguage);
+
+                // Assert
+                Assert.IsTrue((bool)value);
+            }
+        }
+
+        [Test]
+        public void Convert_UInt16ValueAndTheSameStringParameter_True()
+        {
+            // Act
+            var value = this.converter.Convert((ushort)1, BooleanType, "1", EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_EnumToVisibilityConverterSameValue_True()
+        {
+            // Act
+            var value = this.converter.Convert(FocusState.Keyboard, BooleanType, (int)FocusState.Keyboard, EnglishLanguage);
+
+            // Assert
+            Assert.IsTrue((bool)value);
+        }
+
+        [Test]
+        public void Convert_EnumToVisibilityConverterDifferentValue_False()
+        {
+            // Act
+            var value = this.converter.Convert(FocusState.Keyboard, BooleanType, (int)FocusState.Pointer, EnglishLanguage);
+
+            // Assert
+            Assert.IsFalse((bool)value);
+        } 
+    }
+}
---
+++ sources/Windows/OutcoldSolutions.Presentation/Converters/ValueToBooleanConverter.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Outcold Solutions (http://outcoldman.com)
+// --------------------------------------------------------------------------------------------------------------------
+namespace OutcoldSolutions.Converters
+{
+    using System;
+    using System.Globalization;
+
+    using Windows.UI.Xaml.Data;
+
+    /// <summary>
+    /// UI converter to compare value and parameter and return <see cref="bool"/> result. 
+    /// <see cref="VisibilityConverterBase.Invert"/> can invert return values.
+    /// </summary>
+    /// <remarks>
+    /// This converter can be used as a NullToBooleanConverter when parameter is null (not set).
+    /// </remarks>
+    public class ValueToBooleanConverter : IValueConverter
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the value should be inverted.
+        /// </summary>
+        public bool Invert { get; set; }
+
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool result;
+            if (parameter == null)
+            {
+                result = value == null;
+            }
+            else if (value == null)
+            {
+                result = false;
+            }
+            else if (value is double)
+            {
+                var d = System.Convert.ToDouble(parameter, this.GetCultureInfo(language));
+                result = Math.Abs(((double)value) - d) < 0.00001;
+            }
+            else if (value is Enum)
+            {
+                result = value.Equals(Enum.ToObject(value.GetType(), parameter));
+            }
+            else
+            {
+                object parameterValue = System.Convert.ChangeType(parameter, value.GetType(), this.GetCultureInfo(language));
+                result = value.Equals(parameterValue);
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        private CultureInfo GetCultureInfo(string language)
+        {
+            if (string.IsNullOrEmpty(language))
+            {
+                return CultureInfo.CurrentCulture;
+            }
+
+            return new CultureInfo(language);
+        }
+    }
+}
