2dc7a1957607f55eb0a5c1646b5dd82b10b25271
outcoldman <outcoldman@outlook.com>
2013-03-04 22:06:40 -0800

    Fix signing problem

--- nuget.publish/pack.bat
+++ nuget.publish/pack.bat
@@ -1,9 +1,9 @@
 msbuild.exe ..\sources\Framework.sln /property:Configuration=Release /m 
-sn -R "..\bin\Release\Portable\OutcoldSolutions.Diagnostics.ObjectReleaseVerification.dll" D:\SkyDrive\PC\Tools\OutcoldSolutions.snk
-sn -R "..\bin\Release\Portable\OutcoldSolutions.InversionOfControl.dll" D:\SkyDrive\PC\Tools\OutcoldSolutions.snk
-sn -R "..\bin\Release\Portable\OutcoldSolutions.Framework.dll" D:\SkyDrive\PC\Tools\OutcoldSolutions.snk
-sn -R "..\bin\Release\Windows\OutcoldSolutions.Presentation.dll" D:\SkyDrive\PC\Tools\OutcoldSolutions.snk
-sn -R "..\bin\Release\Suites\OutcoldSolutions.SuitesBase.dll" D:\SkyDrive\PC\Tools\OutcoldSolutions.snk
+sn -R "..\bin\Release\Portable\OutcoldSolutions.Diagnostics.ObjectReleaseVerification.dll" %OutcoldTools%\OutcoldSolutions.snk
+sn -R "..\bin\Release\Portable\OutcoldSolutions.InversionOfControl.dll" %OutcoldTools%\OutcoldSolutions.snk
+sn -R "..\bin\Release\Portable\OutcoldSolutions.Framework.dll" %OutcoldTools%\OutcoldSolutions.snk
+sn -R "..\bin\Release\Windows\OutcoldSolutions.Presentation.dll" %OutcoldTools%\OutcoldSolutions.snk
+sn -R "..\bin\Release\Suites\OutcoldSolutions.SuitesBase.dll" %OutcoldTools%\OutcoldSolutions.snk
 nuget pack "..\sources\Portable\OutcoldSolutions.Framework\OutcoldSolutions.Framework.csproj" -basepath "..\bin\Release\Portable" -prop Configuration=Release
 nuget pack "..\sources\Portable\OutcoldSolutions.Diagnostics.ObjectReleaseVerification\OutcoldSolutions.Diagnostics.ObjectReleaseVerification.csproj" -basepath "..\bin\Release\Portable" -prop Configuration=Release
 nuget pack "..\sources\Portable\OutcoldSolutions.InversionOfControl\OutcoldSolutions.InversionOfControl.csproj" -basepath "..\bin\Release\Portable" -prop Configuration=Release
