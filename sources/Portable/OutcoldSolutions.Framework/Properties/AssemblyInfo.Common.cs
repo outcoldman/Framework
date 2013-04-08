f81e2d559cdf85c7ad971996b3e10be151419676
outcoldman <outcoldman@outlook.com>
2013-04-07 23:44:24 -0700

    Update package

--- sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
+++ sources/Portable/OutcoldSolutions.Framework/Properties/AssemblyInfo.Common.cs
@@ -21,5 +21,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.30331.0")]
-[assembly: AssemblyFileVersion("1.0.30331.0")]
+[assembly: AssemblyVersion("1.0.30407.0")]
+[assembly: AssemblyFileVersion("1.0.30407.0")]
